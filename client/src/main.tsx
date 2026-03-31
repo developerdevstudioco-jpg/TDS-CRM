270afbed0150ac2b4e2db5b92a1c7cc0df253589
Raghul Ramesh <rgramesh@sonicwall.com>
2026-03-31 15:15:03 +0530

    Rebuild frontend for latest changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,14 +1,11 @@
-// client/src/App.tsx
+// client/src/main.tsx
 import React from "react";
-import Building2 from "./components/Building2"; // path from src/
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-function App() {
-  return (
-    <div>
-      <h1>My CRM App</h1>
-      <Building2 /> {/* Render the placeholder component */}
-    </div>
-  );
-}
-
-export default App;
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
